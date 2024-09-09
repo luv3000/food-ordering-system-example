8bb875b1e99765c46f9adc4aae022910507e572e
pastak27 <pastak27@gmail.com>
2024-09-09 20:31:09 +0700

    using order payment saga in PaymentResponseMessageListener, implement Order Approval Saga

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/RestaurantApprovalResponseMessageListenerImpl.java
@@ -1,6 +1,7 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval.RestaurantApprovalResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,25 @@ import org.springframework.validation.annotation.Validated;
 @Service
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener {
 
+    private final OrderApprovalSaga orderApprovalSaga;
 
+    public RestaurantApprovalResponseMessageListenerImpl(OrderApprovalSaga orderApprovalSaga) {
+        this.orderApprovalSaga = orderApprovalSaga;
+    }
 
     @Override
     public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
+        orderApprovalSaga.process(restaurantApprovalResponse);
+        log.info("Order is approved for order id {}", restaurantApprovalResponse.getOrderId());
+
     }
 
     @Override
     public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+        OrderCancelledEvent orderCancelledEvent = orderApprovalSaga.rollback(restaurantApprovalResponse);
+        log.info("publishing order cancelled event for order id: {} with failure messages {}",
+                restaurantApprovalResponse.getOrderId(),
+                restaurantApprovalResponse.getFailureMessages());
+        orderCancelledEvent.fire();
     }
 }
